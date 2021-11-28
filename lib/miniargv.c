7e14ea863cd2e647feb03b017f49a2fa713db056
Brecht Sanders <brecht@sanders.org>
2021-11-28 18:25:06 +0100

    add #include <limits.h> for LONG_MIN and LONG_MAX

--- lib/miniargv.c
+++ lib/miniargv.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <limits.h>
 #if defined(_MSC_VER) || (defined(__MINGW32__) && !defined(__MINGW64__))
 #define strcasecmp _stricmp
 #endif
