4e75b18ead800d65ea609fdfd170fcf8e8006f16
brechtsanders <brechtsanders@users.noreply.github.com>
2021-11-28 11:28:46 +0000

    deploy: fe2fbb0ee8d90c2bb620ca12b58be13038b9a40e

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,13 +1,17 @@
 var searchData=
 [
-  ['miniargv_5fenvironment_5fhelp_0',['miniargv_environment_help',['../miniargv_8h.html#a74405c5eeb9a0ab91dd14bd1e10eeb65',1,'miniargv.h']]],
-  ['miniargv_5fget_5fversion_1',['miniargv_get_version',['../miniargv_8h.html#a5101866e681a0bec69a8c2ceb636c7cd',1,'miniargv.h']]],
-  ['miniargv_5fget_5fversion_5fstring_2',['miniargv_get_version_string',['../miniargv_8h.html#a66d2de4b9157a7b30eab56065bfb4649',1,'miniargv.h']]],
-  ['miniargv_5fgetprogramname_3',['miniargv_getprogramname',['../miniargv_8h.html#aa1cf21a3e42ac63ab4dfe44d458658b1',1,'miniargv.h']]],
-  ['miniargv_5fhelp_4',['miniargv_help',['../miniargv_8h.html#aeb5991843f4b5270dcc2838683a99c1e',1,'miniargv.h']]],
-  ['miniargv_5flist_5fargs_5',['miniargv_list_args',['../miniargv_8h.html#a7564fbd9ae14d4da4ffd5533910c3538',1,'miniargv.h']]],
-  ['miniargv_5fprocess_6',['miniargv_process',['../miniargv_8h.html#a0b278aa656fa7e063112df212361d596',1,'miniargv.h']]],
-  ['miniargv_5fprocess_5fenvironment_7',['miniargv_process_environment',['../miniargv_8h.html#a2339a07d55481d0b775c2ee5cd718248',1,'miniargv.h']]],
-  ['miniargv_5fprocess_5fflags_5fonly_8',['miniargv_process_flags_only',['../miniargv_8h.html#a0499007ce0f358d943115f2670862ba1',1,'miniargv.h']]],
-  ['miniargv_5fprocess_5fskip_5fflags_9',['miniargv_process_skip_flags',['../miniargv_8h.html#a71197121795928d55f3855632eecc070',1,'miniargv.h']]]
+  ['miniargv_5farg_5fhelp_0',['miniargv_arg_help',['../miniargv_8h.html#a1f098748c7cfa09a22a5f78adbcf9dee',1,'miniargv.h']]],
+  ['miniargv_5farg_5flist_1',['miniargv_arg_list',['../miniargv_8h.html#a5f40edf85ba2a4d5b4416c7bd599201e',1,'miniargv.h']]],
+  ['miniargv_5fenv_5fhelp_2',['miniargv_env_help',['../miniargv_8h.html#a4d7659faa8dbb2d6a4c6dbe76a55f3f4',1,'miniargv.h']]],
+  ['miniargv_5fenv_5flist_3',['miniargv_env_list',['../miniargv_8h.html#a23c15df98d2fb78a3c32147b1f5e93eb',1,'miniargv.h']]],
+  ['miniargv_5fget_5fversion_4',['miniargv_get_version',['../miniargv_8h.html#a5101866e681a0bec69a8c2ceb636c7cd',1,'miniargv.h']]],
+  ['miniargv_5fget_5fversion_5fstring_5',['miniargv_get_version_string',['../miniargv_8h.html#a66d2de4b9157a7b30eab56065bfb4649',1,'miniargv.h']]],
+  ['miniargv_5fgetprogramname_6',['miniargv_getprogramname',['../miniargv_8h.html#aa1cf21a3e42ac63ab4dfe44d458658b1',1,'miniargv.h']]],
+  ['miniargv_5fhelp_7',['miniargv_help',['../miniargv_8h.html#a6e281f3ed371f8e05a77cdded8594666',1,'miniargv.h']]],
+  ['miniargv_5fprocess_8',['miniargv_process',['../miniargv_8h.html#aff212d68e5ceff2ed20eb660204a71c3',1,'miniargv.h']]],
+  ['miniargv_5fprocess_5farg_9',['miniargv_process_arg',['../miniargv_8h.html#a07b7a401f18615ba4ac31b8271fa6bd3',1,'miniargv.h']]],
+  ['miniargv_5fprocess_5farg_5fflags_10',['miniargv_process_arg_flags',['../miniargv_8h.html#a8f6d14b929f3bf980d99d1d99d2a7a0d',1,'miniargv.h']]],
+  ['miniargv_5fprocess_5farg_5fparams_11',['miniargv_process_arg_params',['../miniargv_8h.html#a98a3d4f90c5edc3fb2581928b3807312',1,'miniargv.h']]],
+  ['miniargv_5fprocess_5fenv_12',['miniargv_process_env',['../miniargv_8h.html#a55bcaa2e259524598275426976285d7b',1,'miniargv.h']]],
+  ['miniargv_5fwrap_5fand_5findent_5ftext_13',['miniargv_wrap_and_indent_text',['../miniargv_8h.html#a3fa56706bc5f1df6b14aaff322007bd4',1,'miniargv.h']]]
 ];
