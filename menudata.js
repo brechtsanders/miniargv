2fa02227c5ae184a9bcac4cb3cf752de43a82718
brechtsanders <brechtsanders@users.noreply.github.com>
2021-12-03 08:49:15 +0000

    deploy: 7e5e3e1b91870ca76b19067d72a930ac5c7b03bf

--- menudata.js
+++ menudata.js
@@ -34,7 +34,8 @@ var menudata={children:[
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
-{text:"All",url:"globals.html"},
+{text:"All",url:"globals.html",children:[
+{text:"m",url:"globals.html#index_m"}]},
 {text:"Functions",url:"globals_func.html"},
 {text:"Typedefs",url:"globals_type.html"},
 {text:"Macros",url:"globals_defs.html"}]}]}]}
