ed3a951d3b6492cb79b1db40eaa1fbcfcb74ad98
brechtsanders <brechtsanders@users.noreply.github.com>
2022-04-18 20:31:52 +0000

    deploy: e2ee2789b19cda0a43298cd9a4e0b2b9aebca97d

--- search/groups_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['miniargv_5fversion_5f_0',['MINIARGV_VERSION_',['../group__MINIARGV__VERSION__.html',1,'']]]
-];
--- search/searchdata.js
+++ search/searchdata.js
@@ -3,12 +3,9 @@ var indexSectionsWithContent =
   0: "achlmsu",
   1: "m",
   2: "m",
-  3: "m",
-  4: "achlsu",
-  5: "m",
-  6: "m",
-  7: "m",
-  8: "m"
+  3: "achlsu",
+  4: "m",
+  5: "m"
 };
 
 var indexSectionNames =
@@ -16,12 +13,9 @@ var indexSectionNames =
   0: "all",
   1: "classes",
   2: "files",
-  3: "functions",
-  4: "variables",
-  5: "typedefs",
-  6: "defines",
-  7: "groups",
-  8: "pages"
+  3: "variables",
+  4: "typedefs",
+  5: "pages"
 };
 
 var indexSectionLabels =
@@ -29,11 +23,8 @@ var indexSectionLabels =
   0: "All",
   1: "Classes",
   2: "Files",
-  3: "Functions",
-  4: "Variables",
-  5: "Typedefs",
-  6: "Macros",
-  7: "Modules",
-  8: "Pages"
+  3: "Variables",
+  4: "Typedefs",
+  5: "Pages"
 };
 
