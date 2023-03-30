d7f39e640ee50637dcfe79b06a67ffea9fb58664
brechtsanders <brechtsanders@users.noreply.github.com>
2023-03-30 21:20:55 +0000

    deploy: 49468b027491e773f5003011ff27755164d45dff

--- search/defines_0.js
+++ search/defines_0.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['miniargv_5fdefinition_5finclude_0',['MINIARGV_DEFINITION_INCLUDE',['../miniargv_8h.html#a4775eed8a4934b35866a85cbba811b1e',1,'miniargv.h']]],
-  ['miniargv_5ffullname_1',['MINIARGV_FULLNAME',['../miniargv_8h.html#a29b1b9d54cc905f724a0b22fc28da679',1,'miniargv.h']]],
-  ['miniargv_5fname_2',['MINIARGV_NAME',['../miniargv_8h.html#a182c25b0186bf7db3821fc2635be4552',1,'miniargv.h']]],
-  ['miniargv_5fversion_3',['MINIARGV_VERSION',['../miniargv_8h.html#a70217c3f2c2b036daa41942882a12c42',1,'miniargv.h']]],
-  ['miniargv_5fversion_5fstring_4',['MINIARGV_VERSION_STRING',['../miniargv_8h.html#ab2da24aecd1eb5de3d102fd0322112e3',1,'miniargv.h']]]
+  ['miniargv_5fdefinition_5fend_0',['MINIARGV_DEFINITION_END',['../miniargv_8h.html#aa385dda0d434995cd41916a02cb81862',1,'miniargv.h']]],
+  ['miniargv_5fdefinition_5finclude_1',['MINIARGV_DEFINITION_INCLUDE',['../miniargv_8h.html#a4775eed8a4934b35866a85cbba811b1e',1,'miniargv.h']]],
+  ['miniargv_5ffullname_2',['MINIARGV_FULLNAME',['../miniargv_8h.html#a29b1b9d54cc905f724a0b22fc28da679',1,'miniargv.h']]],
+  ['miniargv_5fname_3',['MINIARGV_NAME',['../miniargv_8h.html#a182c25b0186bf7db3821fc2635be4552',1,'miniargv.h']]],
+  ['miniargv_5fversion_4',['MINIARGV_VERSION',['../miniargv_8h.html#a70217c3f2c2b036daa41942882a12c42',1,'miniargv.h']]],
+  ['miniargv_5fversion_5fstring_5',['MINIARGV_VERSION_STRING',['../miniargv_8h.html#ab2da24aecd1eb5de3d102fd0322112e3',1,'miniargv.h']]]
 ];
--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['miniargv_5fdefinition_0',['miniargv_definition',['../miniargv_8h.html#a7638c7e1f2040ce99ebc4ffca27e7fff',1,'miniargv.h']]],
-  ['miniargv_5fhandler_5ffn_1',['miniargv_handler_fn',['../miniargv_8h.html#a07fb665b9d40f22c4d349e3f1b77d35d',1,'miniargv.h']]]
+  ['miniargv_5fcomplete_5ffn_0',['miniargv_complete_fn',['../miniargv_8h.html#a812fbe87ca33fd3c436bfff1ddd41024',1,'miniargv.h']]],
+  ['miniargv_5fdefinition_1',['miniargv_definition',['../miniargv_8h.html#a7638c7e1f2040ce99ebc4ffca27e7fff',1,'miniargv.h']]],
+  ['miniargv_5fhandler_5ffn_2',['miniargv_handler_fn',['../miniargv_8h.html#a07fb665b9d40f22c4d349e3f1b77d35d',1,'miniargv.h']]]
 ];
