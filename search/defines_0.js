004c32fe20e5d3532affc236ee47395b0ebaf209
brechtsanders <brechtsanders@users.noreply.github.com>
2024-01-29 20:35:19 +0000

    deploy: 3237e69327aa693a25a6d23b891fa3bbcace3dbe

--- search/defines_0.js
+++ search/defines_0.js
@@ -3,7 +3,8 @@ var searchData=
   ['miniargv_5fdefinition_5fend_0',['MINIARGV_DEFINITION_END',['../miniargv_8h.html#aa385dda0d434995cd41916a02cb81862',1,'miniargv.h']]],
   ['miniargv_5fdefinition_5finclude_1',['MINIARGV_DEFINITION_INCLUDE',['../miniargv_8h.html#a4775eed8a4934b35866a85cbba811b1e',1,'miniargv.h']]],
   ['miniargv_5ffullname_2',['MINIARGV_FULLNAME',['../miniargv_8h.html#a29b1b9d54cc905f724a0b22fc28da679',1,'miniargv.h']]],
-  ['miniargv_5fname_3',['MINIARGV_NAME',['../miniargv_8h.html#a182c25b0186bf7db3821fc2635be4552',1,'miniargv.h']]],
-  ['miniargv_5fversion_4',['MINIARGV_VERSION',['../miniargv_8h.html#a70217c3f2c2b036daa41942882a12c42',1,'miniargv.h']]],
-  ['miniargv_5fversion_5fstring_5',['MINIARGV_VERSION_STRING',['../miniargv_8h.html#ab2da24aecd1eb5de3d102fd0322112e3',1,'miniargv.h']]]
+  ['miniargv_5fignore_5ferrors_3',['MINIARGV_IGNORE_ERRORS',['../miniargv_8h.html#a7f9f3d154abce7707f653a8e50cb3b1d',1,'miniargv.h']]],
+  ['miniargv_5fname_4',['MINIARGV_NAME',['../miniargv_8h.html#a182c25b0186bf7db3821fc2635be4552',1,'miniargv.h']]],
+  ['miniargv_5fversion_5',['MINIARGV_VERSION',['../miniargv_8h.html#a70217c3f2c2b036daa41942882a12c42',1,'miniargv.h']]],
+  ['miniargv_5fversion_5fstring_6',['MINIARGV_VERSION_STRING',['../miniargv_8h.html#ab2da24aecd1eb5de3d102fd0322112e3',1,'miniargv.h']]]
 ];
